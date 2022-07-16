5474d6a535a5f3b0256c64aa5acc77765bd41986
Prathamesh Gosavi <prathameshgosavi015@gmail.com>
2022-07-17 02:34:19 +0530

    Inital Commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:facebook_responsive_ui_flutter/config/palette.dart';
+import 'package:facebook_responsive_ui_flutter/screens/screens.dart';
+import 'package:flutter/material.dart';
+
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Facebook UI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        scaffoldBackgroundColor: Palette.scaffold,
+      ),
+      home: NavScreen(),
+    );
+  }
+}
